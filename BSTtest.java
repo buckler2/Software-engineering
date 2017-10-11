4e9114769029ed294295079306646869490375bf
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-11 15:56:07 +0100

    Add files via upload

---
+++ BSTtest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BSTtest {
+
+	@Test
+	//test If node in question is actually in the binary search tree
+	public void testNodeInTree(){
+		
+	}
+	@Test
+	//Test if there are any nodes in the binary search tree yet
+	public void testisEmpty(){
+		
+	}
+	
+	@Test
+	//Test that the nodes selected exist and are suitable
+	//Ensure the root node has not been selected as it cannot
+	//have an ancestor node
+	public void testSelection(){
+		
+	}
+	
+	@Test
+	//Test if the function to retrieve a node returns the right value
+	public void testGetNode(){
+		
+	}
+	
+	@Test
+	//ensure the returned node is an actual ancestor
+	public void testAncestorNode() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	//Test if Node is the lowest common ancestor
+	public void testIsLowestCommonAncestor(){
+		
+	}
+	
+
+}
