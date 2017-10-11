39a73d213d06ec37023672a008994f485fddf3c6
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-11 16:04:15 +0100

    initial set up of BST class

---
+++ BST.java
@@ -0,0 +1,26 @@
+
+
+	import java.util.NoSuchElementException;
+public class BST {
+    
+	public boolean isEmpty()
+	{
+		
+	}
+	
+	public boolean containsNode()
+	{
+		
+	}
+	
+	public Node get (Node x)
+	{
+		
+	}
+	
+	public Node findAncestorNode (Node x, Node y)
+	{
+		
+	}
+	
+}
