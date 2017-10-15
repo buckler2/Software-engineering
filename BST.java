db94b245c395538cb53ea6b8b4eb53b0d7447bbb
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-15 16:24:46 +0100

    Get function implemented

--- BST.java
+++ BST.java
@@ -64,17 +64,16 @@ public class BST<Key extends Comparable<Key>, Value> {
         if (x == null) return 0;
         else return x.N;
     }
-	
-	private Value get(Node x,Key k)
-	{
-	        if (x == null) return null;
-	        int cmp = k.compareTo(x.key);
-	        if      (cmp < 0) return get(x.left, k);
-	        else if (cmp > 0) return get(x.right, k);
-	        else              return x.val;
-	    
-	}
-	
+    public Value get(Key key) { return get(root, key); }
+
+    private Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else              return x.val;
+    }
+
 	public Node lca(Node x, Node y)
 	{
 		//first assign lca to root as if all options fail this is the Lca
