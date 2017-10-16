25f218a515b100f93f3531f0ac5332be9654f586
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-16 13:01:21 +0100

    get function removed and lowest common ancestor function implemented

--- BST.java
+++ BST.java
@@ -12,12 +12,10 @@ public class BST<Key extends Comparable<Key>, Value> {
         private Key key;           // sorted by key
         private Value val;         // associated data
         private Node left, right;  // left and right subtrees
-        private int N;             // number of nodes in subtree
 
-        public Node(Key key, Value val, int N) {
+        public Node(Key key, Value val) {
             this.key = key;
             this.val = val;
-            this.N = N;
         }
     }
     
@@ -30,12 +28,7 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return false;
 	}
 	
-	public boolean containsNode(Node x)
-	{
-		while(root.)
-		
-	}
-	
+
 	
 	public void put(Key key, Value val) {
         if (val == null) { return; } //something missing here
@@ -43,42 +36,45 @@ public class BST<Key extends Comparable<Key>, Value> {
     }
 
     private Node put(Node x, Key key, Value val) {
-        if (x == null) return new Node(key, val, 1);
+        if (x == null) return new Node(key, val);
         int cmp = key.compareTo(x.key);
         if      (cmp < 0) x.left  = put(x.left,  key, val);
         else if (cmp > 0) x.right = put(x.right, key, val);
         else              x.val   = val;
-        x.N = 1 + size(x.left) + size(x.right);
         return x;
     }
-    
-    // return number of key-value pairs in BST
-    public int size() 
-    { 
-    	return size(root); 
-    }
-
-    // return number of key-value pairs in BST rooted at x
-    private int size(Node x) 
-    {
-        if (x == null) return 0;
-        else return x.N;
-    }
-    public Value get(Key key) { return get(root, key); }
-
-    private Value get(Node x, Key key) {
-        if (x == null) return null;
-        int cmp = key.compareTo(x.key);
-        if      (cmp < 0) return get(x.left, key);
-        else if (cmp > 0) return get(x.right, key);
-        else              return x.val;
-    }
-
-	public Node lca(Node x, Node y)
+   
+	public Node lca(Node r,Node x, Node y)
 	{
 		//first assign lca to root as if all options fail this is the Lca
 		Node lowestCommonAncestor = root;
-		return; 
+		r = root;
+		
+		 if(isEmpty())
+		 {
+			 return null; 
+		 }
+		 if(root== x || root==y)
+		 {
+			 return root;
+		 }
+		 
+		 Node p = lca(root.left, x, y);
+		 Node w = lca(root.right, x, y);
+		 
+		    if(p!=null && w!=null){
+		        return root;
+		    }
+		    else if
+		    (p==null && w==null)
+		    {
+		    	lowestCommonAncestor = null;
+		    }
+		    else
+		    {
+		    	lowestCommonAncestor = p==null?w:p;
+		    } 
+		    return lowestCommonAncestor;
 	}
 	
 }
