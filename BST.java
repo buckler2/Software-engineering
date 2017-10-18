443c783a144504e2c5775b62140508d99117b13b
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-18 22:16:27 +0100

    Add files via upload

--- BST.java
+++ BST.java
@@ -2,7 +2,7 @@ import java.util.NoSuchElementException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BST<Key extends Comparable<Key>, Value> {
+public class BST <Key extends Comparable<Key>> {
 	
 	private Key [] keysInOrder;
     private Node root;             // root of BST
@@ -10,43 +10,59 @@ public class BST<Key extends Comparable<Key>, Value> {
     /**
      * Private node class.
      */
-    private class Node {
-    	int key;
+    public class Node {
+    	Key key;
         Node left, right, parent;
      
-        Node(int key) 
+        Node(Key key) 
         {
             this.key = key;
             left = right = parent = null;
         }
-    }
-    
 
-    Node insert(Node node, int key) 
+    }
+    public void insert(Key key){
+    	root = insert(root, key);
+
+    }
+    //taken from code I wrote last year
+    public boolean contains(Key key)
+    { return contains(root, key); }
+
+    private boolean contains(Node x, Key key) {
+        if (x == null) return false;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return contains(x.left, key);
+        else if (cmp > 0) return contains(x.right, key);
+        else              return true;
+    }
+
+    private Node insert(Node node, Key key) 
     {
         /* If the tree is empty, return a new node */
         if (node == null)
             return new Node(key);
- 
-        /* Otherwise, recur down the tree */
-        if (key < node.key) 
-        {
-            node.left = insert(node.left, key);
-            node.left.parent = node;
-        }
-        else if (key > node.key) 
-        {
-            node.right = insert(node.right, key);
-            node.right.parent = node;
-        }
- 
-        /* return the (unchanged) node pointer */
+       
+        int cmp = key.compareTo(node.key);
+		if(cmp<0)
+		{
+			node.left = insert(node.left, key);
+		}
+		else if(cmp>0)
+		{
+			node.right = insert(node.right, key);
+		} 
         return node;
     }
  
    
 	public Node lca(Node r,Node x, Node y)
 	{
+		//ensures that both nodes are in the search tree
+		if(!this.contains(x.key) || !this.contains(y.key))
+		{
+			return null;
+		}
 		if(root==null || x==null || y == null)
 	        return null;
 	 
@@ -57,15 +73,17 @@ public class BST<Key extends Comparable<Key>, Value> {
 	    Node l = lca(root.left, x, y);
 	    Node p = lca(root.right, x, y);
 	 
-	    if(l!=null&&r!=null)
+	    if(l!=null&&p!=null)
 	    {
 	        return root;
-	    }else if(l==null&&r==null)
+	    }
+	    else if(l==null&&p==null)
 	    {
 	        return null;
-	    }else
+	    }
+	    else
 	    {
-	        return l==null?r:l;
+	        return l==null?p:l;
 	    }
 	}
 }
--- BSTtest.java
+++ BSTtest.java
@@ -1,39 +1,62 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 public class BSTtest {
 
 	
 	@Test 
 	public void testInsert(){
-		BST<Integer, Integer> bst = new BST<Integer, Integer>();
-		BST<Integer, Integer> bst = new BST<Integer, Integer>();
-		bst.insert(7, 7);   //        _7_
-		bst.insert(8, 8);   //      /     \
-		bst.insert(3, 3);   //    _3_      8
-		bst.insert(1, 1);   //  /     \
-		bst.insert(2, 2);   // 1       6
-		bst.insert(6, 6);   //  \     /
-		bst.insert(4, 4);   //   2   4
-		bst.insert(5, 5);   //        \
-						 /
-		assertTrue("Checking the put value of the tree", bst.contains(7));
-		assertFalse("Checking the put value of the tree", bst.contains(4));
+		BST<Integer> bst = new BST<Integer>();
+		bst.insert(7);   //        _7_
+		bst.insert(8);   //      /     \
+		bst.insert(3);   //    _3_      8
+		bst.insert(1);   //  /     \
+		bst.insert(2);   // 1       6
+		bst.insert(6);   //  \     /
+		bst.insert(4);   //   2   4
+		bst.insert(5);   //        \
+		
+		assertTrue("Checking the insert value of the tree", (bst.contains(7)));
+		assertTrue("Checking the insert value of the tree", bst.contains(5));
+		assertFalse("Checking the insert value of the tree",bst.contains(51));
+	}
+	
+	@Test
+	public void testContains(){
+		BST<Integer> bst = new BST<Integer>();
+		bst.insert(7);   //        _7_
+		bst.insert(8);   //      /     \
+		bst.insert(3);   //    _3_      8
+		bst.insert(1);   //  /     \
+		bst.insert(2);   // 1       6
+		bst.insert(6);   //  \     /
+		bst.insert(4);   //   2   4
+		bst.insert(5);   //        \
+		
+		assertEquals("Checking the get value of the tree", true, bst.contains(1));
+		BST<Integer> bst1 = new BST<Integer>();
+		bst1.insert(7);
+		assertEquals("Checking the get for only one root node", true, bst1.contains(7));
+		BST<Integer> bst2 = new BST<Integer>();
+		assertEquals("Checking the get for empty", false, bst2.contains(null));
+		
 	}
 	
 	public void testLowestCommonAncestor(){
-		bst.insert(7, 7);   //        _7_
-		bst.insert(8, 8);   //      /     \
-		bst.insert(3, 3);   //    _3_      8
-		bst.insert(1, 1);   //  /     \
-		bst.insert(2, 2);   // 1       6
-		bst.insert(6, 6);   //  \     /
-		bst.insert(4, 4);   //   2   4
-		bst.insert(5, 5);   //  
+		BST<Integer> bst = new BST<Integer>();
+		bst.insert(7);   //        _7_
+		bst.insert(8);   //      /     \
+		bst.insert(3);   //    _3_      8
+		bst.insert(1);   //  /     \
+		bst.insert(2);   // 1       6
+		bst.insert(6);   //  \     /
+		bst.insert(4);   //   2   4
+		bst.insert(5);   //  
 		
-		findLCA();
-		findLCA()
+		//findLCA();
+	//	findLCA()
 		
 	}
 	
