3df37a2a6073bfe243b7298b81c2aa98aec1ab2b
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-10-31 13:10:36 +0000

    Add files via upload

--- LCADag.java
+++ LCADag.java
@@ -1,36 +1,50 @@
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public class LCADag {
 	
-	private int vertices;           // number of vertices in this digraph
-	private int edges;                 // number of edges in this digraph
-	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
-	private int[] indegree;        // indegree[v] = indegree of vertex v
-	
-	private boolean marked[];		//Boolean List to track visited vertices
-	private boolean cycleExists;		//True if cycle in graph
-    private boolean stack[];		//Order that vertices were visited
-
-	
-	public LCADag(int V)
-	{
-		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
-	    this.vertices = V;
-	    this.edges = 0;
-	    indegree = new int[V];
-	    marked = new boolean[V];
-	    stack = new boolean[V];
-	    adj = (ArrayList<Integer>[]) new ArrayList[V];
-	    for (int v = 0; v < V; v++) {
-	        adj[v] = new ArrayList<Integer>();
-	    }              
+	public class Node{
+    	int data;
+     
+        Node(int value) 
+        {
+            data =value;
+        }
+        
+        List<Node> parents = new ArrayList<Node>();
+        List<Node> children = new ArrayList<Node>();
+        public void addParent(Node x)
+        {
+        	this.parents.add(x);
+        }
+        public void addChild(Node y)
+        {
+        	this.children.add(y);
+        }
 	}
-
-
+	
+	public int LCA(Node x, Node y){
+		List<Node> xParents = x.parents;
+		List<Node> yParents = y.parents;
+		
+		int i = xParents.size();
+		int j = yParents.size();
+		for(; i >=0 ; i--)
+		{
+			for(; j>=0; i--)
+			{
+				if(xParents.get(i) == xParents.get(j))
+				{
+					
+				}
+			}
+		}
+		return -1;
+	}
+	
 
 }
