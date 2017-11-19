a1ce3235a0a65e7627617312deb57948abb90c07
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-11-19 16:56:16 +0000

    Add files via upload

--- API.R
+++ API.R
@@ -1,4 +1,12 @@
 library(httr);
 
-query <- "https://api.github.com/orgs/github"
-out = GET(url = query);
+'Access token = 15050eb5dcf2a17e95252d709911e4f7a347929d'
+'https://developer.github.com/v4/guides/intro-to-graphql/'
+
+query <- "https://api.github.com/orgs/github";
+query2 <- "https://api.github.com/users/octocat";
+query3 <- "https://api.github.com/teams/1";
+
+curl = "15050eb5dcf2a17e95252d709911e4f7a347929d https://api.github.com/graphql";
+out = GET(url = curl);
+
