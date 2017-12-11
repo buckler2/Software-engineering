15d76ea0ffc47eb0f11a602bc51d2dfb0c1df9a2
buckler2 <32679168+buckler2@users.noreply.github.com>
2017-12-11 17:46:23 +0000

    Add files via upload

--- API.R
+++ API.R
@@ -2,15 +2,27 @@ library(jsonlite)
 library(httpuv)
 library(httr)
 
-oauth_endpoints("github")
-myapp <- oauth_app(appname = "Software Engineering", key = "f7e905beb15f09442c4a",
-                     secret = "03bef356950401e16edfd6f979f7c74e7087fb73")
+'initial set up taken from
+https://towardsdatascience.com/accessing-data-from-github-api-using-r-3633fb62cb08'
 
-gh_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+oauth_endpoints("github")
+app <- oauth_app(appname = "Software Engineering", key = "97c3d9beb438eadd730e",
+                     secret = "201bb4ee8eb0618f9797d30acef5355553243e67")
+
+gh_token <- oauth2.0_token(oauth_endpoints("github"),app)
 token <- config(token = gh_token)
 
-getrepo <- GET("https://api.github.com/users/buckler2/repos", token)
+request <- GET("https://api.github.com/users/buckler2/repos", token)
 
+#take on action httr error
 stop_for_status(request)
-json1 = content(request)
+
+#extraxt content from a request
+json1 <- content(request)
+
+
+#convert to a data frame
 gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
